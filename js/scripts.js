e4c3e111c489d7182094a7bfe4ea6ae96cd071bd
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2022-12-21 11:24:02 +0300

    implement javascript linting

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
-const body = document.body;
-const brand = document.getElementById("brand");
-const humberger = document.getElementById("humberger");
-const humbergerClose = document.getElementById("humberger-close");
-const navigation = document.querySelector("nav");
-const navToggle = document.getElementById("nav-toggle");
+const brand = document.getElementById('brand');
+const humberger = document.getElementById('humberger');
+const humbergerClose = document.getElementById('humberger-close');
+const navigation = document.querySelector('nav');
+const navToggle = document.getElementById('nav-toggle');
 
-function handleSidebar(flag){
-  if(flag===true){
-     navigation.style = "transform:scale(1,1)";
-     brand.style = "visibility:hidden;";
-     humberger.style = "display:none";
-     humbergerClose.style = "display:block";
-  }else{
-    navigation.style = "transform:scale(1,0)";
-    brand.style = "visibility:visible;";
-    humberger.style = "display:block";
-    humbergerClose.style = "display:none";
+function handleSidebar(flag) {
+  if (flag === true) {
+    navigation.style = 'transform:scale(1,1)';
+    brand.style = 'visibility:hidden;';
+    humberger.style = 'display:none';
+    humbergerClose.style = 'display:block';
+  } else {
+    navigation.style = 'transform:scale(1,0)';
+    brand.style = 'visibility:visible;';
+    humberger.style = 'display:block';
+    humbergerClose.style = 'display:none';
   }
 }
 
-navToggle.addEventListener("change", (e) => {
+navToggle.addEventListener('change', (e) => {
   if (e.target.checked === true) {
     handleSidebar(true);
   } else {
     handleSidebar(false);
   }
 });
-
-
-
-
