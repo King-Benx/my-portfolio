eb0f41678dbcd63e5dc2354a6a452f8b8e100d71
Benard Asiimwe Bataringaya <benard.asiimwe.bataringaya@gmail.com>
2022-12-21 11:20:48 +0300

    implement open collapse navigation

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,32 @@
 const body = document.body;
-
+const brand = document.getElementById("brand");
+const humberger = document.getElementById("humberger");
+const humbergerClose = document.getElementById("humberger-close");
+const navigation = document.querySelector("nav");
 const navToggle = document.getElementById("nav-toggle");
 
-const brand = document.getElementById("brand");
-
-const humberger = document.getElementById("humberger");
-
-const humbergerClose = document.getElementById("humberger-close");
-
-const navigation = document.querySelector("nav");
-
-navToggle.addEventListener("change", (e) => {
-  if (e.target.checked === true) {
-    navigation.style = "transform:scale(1,1)";
-    brand.style = "visibility:hidden;";
-    humberger.style = "display:none";
-    humbergerClose.style = "display:block";
-  } else {
+function handleSidebar(flag){
+  if(flag===true){
+     navigation.style = "transform:scale(1,1)";
+     brand.style = "visibility:hidden;";
+     humberger.style = "display:none";
+     humbergerClose.style = "display:block";
+  }else{
     navigation.style = "transform:scale(1,0)";
     brand.style = "visibility:visible;";
     humberger.style = "display:block";
     humbergerClose.style = "display:none";
   }
+}
+
+navToggle.addEventListener("change", (e) => {
+  if (e.target.checked === true) {
+    handleSidebar(true);
+  } else {
+    handleSidebar(false);
+  }
 });
+
+
 
 
